f76abc10f8eb5ddeaff4d711453a7033f6802aed
yenshipotato <jihmjihh@gmail.com>
2024-02-28 20:28:29 +0800

    Fix q_insert_head

--- queue.c
+++ queue.c
@@ -33,6 +33,15 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    element_t *node = malloc(sizeof(element_t));
+    if (!node)
+        return false;
+    if ((node->value = strdup(s)) != NULL)
+        list_add(&node->list, head);
+    else {
+        free(node);
+        return false;
+    }
     return true;
 }
 
